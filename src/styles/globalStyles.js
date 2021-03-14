b95c9cd8a6a121b54a8a031df94be2809ce2f9f6
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-03-14 19:46:18 -0300

    Add theme and global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: ${theme.fonts.fontFamily};
+	font-weight: 400;
+	color: ${theme.fonts.primaryColor}
+};
+body {
+	height: 100vh;
+	padding: 0.5rem;
+	background-color: #faf8f8;
+}
+`;
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+	colors: {
+		desktopGradient:
+			"linear-gradient(90deg, rgba(239,129,170,1) 0%, rgba(239,168,131,1) 100%)",
+		mobileGradient:
+			"linear-gradient(0deg, rgba(239,129,170,1) 0%, rgba(239,168,131,1) 100%)",
+		darkBoxShadow: "0px 0px 10px 0px #D9ADA9"
+	},
+
+	fonts: {
+		primaryColor: "#656565",
+		secondaryColor: "#D98162",
+		fontFamily: "Poppins, sans-serif"
+	},
+
+	mediaQueries: {
+		above480: "(min-width: 480px)",
+		above600: "(min-width: 600px)",
+		above768: "(min-width: 768px)",
+		above900: "(min-width: 900px)",
+		above1024: "(min-width: 1024px)",
+		above1200: "(min-width: 1200px)"
+	}
+};
+
+export default theme;
