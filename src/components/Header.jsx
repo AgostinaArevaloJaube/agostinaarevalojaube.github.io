e39a17eefa2316173d52e9c34e5a983b8c1a0522
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-03-14 19:51:57 -0300

    Create header and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import { HashLink } from "react-router-hash-link";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import Navbar from "./Navbar";
+
+function Header() {
+	return (
+		<HeaderContainer>
+			<Title>
+				<HashLinks to="#hero">
+					Agostina <span>Arevalo Jaube</span>
+				</HashLinks>
+			</Title>
+			<Navbar />
+		</HeaderContainer>
+	);
+}
+
+const HeaderContainer = styled.header`
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	padding: 0.5rem;
+	height: 7vh;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	box-shadow: ${theme.colors.darkBoxShadow};
+`;
+
+const Title = styled.h2`
+	background: ${theme.colors.desktopGradient};
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+
+	@media ${theme.mediaQueries.above768} {
+		color: ${theme.fonts.primaryColor};
+	}
+	span {
+		display: none;
+
+		@media ${theme.mediaQueries.above768} {
+			display: inline;
+			font-weight: 500;
+			background: ${theme.colors.desktopGradient};
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
+		}
+	}
+`;
+
+const HashLinks = styled(HashLink)`
+	text-decoration: none;
+`;
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import BurgerOpen from "../assets/burger.svg";
+import BurgerClosed from "../assets/burger-closed.svg";
+import styled from "styled-components";
+import theme from "../styles/theme";
+
+function Navbar() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleClosingNavbar = () => setIsOpen(false);
+
+	return (
+		<nav>
+			<Hamburger onClick={() => setIsOpen(!isOpen)}>
+				{isOpen ? (
+					<img src={BurgerClosed} alt="Cerrar menu" />
+				) : (
+					<img src={BurgerOpen} alt="Abrir menu" />
+				)}
+			</Hamburger>
+			<MenuList isOpen={isOpen}>
+				<MenuLinks onClick={handleClosingNavbar}>
+					<HashLinks to="#about">Sobre mi</HashLinks>
+				</MenuLinks>
+				<MenuLinks onClick={handleClosingNavbar}>
+					<HashLinks to="#stack">Tecnologías</HashLinks>
+				</MenuLinks>
+				<MenuLinks onClick={handleClosingNavbar}>
+					<HashLinks to="#proyectos">Proyectos</HashLinks>
+				</MenuLinks>
+				<MenuLinks onClick={handleClosingNavbar}>
+					<HashLinks to="#hero">Contacto</HashLinks>
+				</MenuLinks>
+			</MenuList>
+		</nav>
+	);
+}
+
+const Hamburger = styled.div`
+	margin: 0.5rem;
+	width: 1.4rem;
+	cursor: pointer;
+	@media ${theme.mediaQueries.above600} {
+		display: none;
+	}
+`;
+
+const MenuList = styled.ul`
+	position: absolute;
+	top: 3.8rem;
+	left: 0;
+	width: 100%;
+	height: ${({ isOpen }) => (isOpen ? "100vh" : "0")};
+	transition: ${({ isOpen }) => (isOpen ? "height 0.4s ease-in" : "none")};
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+	background: ${theme.colors.mobileGradient};
+	color: #fff;
+
+	@media ${theme.mediaQueries.above600} {
+		position: relative;
+		top: 0;
+		height: 100%;
+		flex-direction: row;
+		background: transparent;
+	}
+`;
+
+const MenuLinks = styled.li`
+	padding: 2rem 0.5rem;
+	width: 100%;
+	text-align: center;
+	list-style: none;
+	cursor: pointer;
+
+	@media ${theme.mediaQueries.above600} {
+		width: fit-content;
+		padding: 0.5rem;
+	}
+`;
+
+const HashLinks = styled(HashLink)`
+	text-decoration: none;
+	color: #fff;
+
+	@media ${theme.mediaQueries.above600} {
+		color: ${theme.fonts.primaryColor};
+
+		&:hover{
+			transition: font-size 0.2s ease-in-out;
+			font-size: 18px;
+			border-radius: 1rem;
+			color: #fff;
+			background: ${theme.colors.desktopGradient};
+			-webkit-background-clip: text;
+  		-webkit-text-fill-color: transparent;
+		}
+`;
+
+export default Navbar;
