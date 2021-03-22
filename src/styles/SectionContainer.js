8a64fa2814ac85c30e5ca99500e48c39895484de
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-03-22 19:44:50 -0300

    Add section container's style

---
+++ src/styles/SectionContainer.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SectionContainer = styled.section`
+	height: 93vh;
+	max-height: 93vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+export default SectionContainer;
